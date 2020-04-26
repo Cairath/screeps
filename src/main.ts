f27fd7c58272f4827d87231e4deffb3d0ed70a0b
Cairath <10224213+Cairath@users.noreply.github.com>
2020-04-26 04:13:58 +0200

    Removing memory of dead creeps

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ const managers: Manager[] = [];
 managerRooms.forEach((room: string) => managers.push(new Manager(room)));
 
 export const loop = ErrorMapper.wrapLoop(() => {
+  for (const creepName in Memory.creeps) {
+    if (!(creepName in Game.creeps)) {
+      delete Memory.creeps[creepName];
+    }
+  }
+
   managers.forEach((manager: Manager) => manager.manage());
 });
 
