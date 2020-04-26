7e3a985e52b7b138d140c11db0e2e6cac8bb49e4
Cairath <10224213+Cairath@users.noreply.github.com>
2020-04-26 16:15:21 +0200

    Basic ideas for the manager

--- src/manager/index.ts
+++ src/manager/index.ts
@@ -1,12 +1,32 @@
+import _ from "lodash";
+
 class Manager {
   room: string;
+  // additionalRooms: string[] -- rooms to manage for mining resources. possibly a dictionary or a record of room name,
+  // and specifying whether type of management (mining sources / minerals / both / something else???)
 
   constructor(room: string) {
     this.room = room;
   }
 
   manage() {
-    // empty
+    const roomCreeps = _.filter(Game.creeps, (creep: Creep) => creep.memory.room === this.room);
+    // handle creep numbers. calculate how many of which need to be alive and adjust accordingly
+
+    // check if there are any priority jobs, such as gathering loot that is decaying, critical repair etc.
+    // construct a list of those jobs grouped by job type / role
+
+    const idleCreeps = _.filter(roomCreeps, (creep: Creep) => !creep.memory.working);
+
+    if (!idleCreeps || idleCreeps.length === 0) {
+      // if there are priority jobs and no idle creep for a given job, grab the most fitting busy creep and make him do that
+
+      return;
+    }
+    /* if there are any idle creeps
+        - construct a list of available idle roles (dictionary of role - count?)
+        - find and assign jobs for the list above
+    */
   }
 }
 
