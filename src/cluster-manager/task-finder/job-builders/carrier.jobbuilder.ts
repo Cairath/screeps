2c7c56412a1815e62add971e20faf68086c12bef
Cairath <10224213+Cairath@users.noreply.github.com>
2020-05-09 15:43:48 +0200

    Basic Carrier Job Builder - emptying containers with Empty mode

--- src/cluster-manager/task-finder/job-builders/carrier.jobbuilder.ts
+++ src/cluster-manager/task-finder/job-builders/carrier.jobbuilder.ts
@@ -2,27 +2,64 @@ import { ClusterManager } from "cluster-manager";
 import _ from "lodash";
 import { JobBuilder } from "./JobBuilder";
 import { StorageController } from "cluster-manager/storage-controller/StorageController";
+import { RoleBodyConfigurations } from "creeps";
+import * as storageUtils from "../../storage-controller/utils";
 
 export class CarrierJobBuilder extends JobBuilder {
   private clusterManager: ClusterManager;
-  private storageController: StorageController;
 
   constructor(clusterManager: ClusterManager) {
     super();
 
     this.clusterManager = clusterManager;
-    this.storageController = clusterManager.storageController;
   }
 
   public buildJobList(): Job[] {
     let jobs: Job[] = [];
 
+    const storageController = this.clusterManager.storageController;
+
+    const storesWithEmptyMode = storageController.getStores(STORAGE_MODE_EMPTY);
+    const storesWithFillMode = storageController.getStores(STORAGE_MODE_FILL);
+    const storesWithNormalMode = storageController.getStores(STORAGE_MODE_FILL);
+    const storesWithoutEmptyMode = [...storesWithFillMode, ...storesWithNormalMode];
+
+    const carryCapacitiesAtCurrentTier =
+      _.filter(
+        RoleBodyConfigurations.carrier[this.clusterManager.creepsTier],
+        (part: BodyPartConstant) => part === CARRY
+      ).length * CARRY_CAPACITY;
+
+    storesWithEmptyMode.forEach((object: StructureWithStoreDefinition | Tombstone | Ruin) => {
+      const store = object.store as StoreDefinition;
+      if (store.getUsedCapacity() < carryCapacitiesAtCurrentTier) {
+        return;
+      }
+
+      const resourcesInStore = storageUtils.getResourcesInStore(store);
+
+      (Object.keys(resourcesInStore) as ResourceConstant[]).forEach((resource: ResourceConstant) => {
+        const amount = resourcesInStore[resource];
+
+        if (!amount || amount < carryCapacitiesAtCurrentTier) {
+          return;
+        }
+
+        const withdrawJob: WithdrawJob = {
+          type: TASK_WITHDRAW,
+          priority:
+            store.getFreeCapacity(resource) / store.getCapacity(resource) > 0.6 ? PRIORITY_HIGH : PRIORITY_NORMAL,
+          objectId: object.id,
+          resource: resource,
+          amount: amount
+        };
+
+        jobs.push(withdrawJob);
+      });
+    });
+
     jobs = _.orderBy(jobs, (j: Job) => j.priority, "desc");
-
+    console.log(JSON.stringify(jobs, null, 2));
     return jobs;
-  }
-
-  protected findPriorityJobs(): Job[] {
-    return [];
   }
 }
